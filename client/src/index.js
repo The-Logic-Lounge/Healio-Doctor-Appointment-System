41478e1d6df6fdfc8904847c16fc8171d7a22bc7
umEngineer-ai <umemaryam.engineer@gmail.com>
2026-09-12 14:03:46 +0500

    Fix production authentication configuration

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 // Configure default baseURL for API requests. 
 // In development, it defaults to "" (using package.json proxy). 
 // In production, it uses REACT_APP_API_URL.
 axios.defaults.baseURL = process.env.REACT_APP_API_URL || "";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
